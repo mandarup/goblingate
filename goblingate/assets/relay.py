6fc34ba801a8c535f23b82abb101d0c6846286fa
Mandar Upadhye <mandarupadhye@gmail.com>
2018-12-16 16:13:16 +0000

    add relay signal attribute == not ON

--- goblingate/assets/relay.py
+++ goblingate/assets/relay.py
@@ -13,12 +13,14 @@ class Relay(object):
         self.stop_time = None
         self._duration = duration
         self.history = []
+        self.signal = True # True is Normally Open (i.e. switch OFF)
 
     def reset(self):
         self.is_on = False
         self.start_time = None
         self.stop_time = None
         self.history = []
+        self.signal = True
 
     def update(self, signal):
         if self.is_on and signal:
@@ -26,6 +28,8 @@ class Relay(object):
         self.is_on = signal
         if self.is_on:
             self.start_time = dt.datetime.now()
+            self.signal = False
         else:
             self.stop_time = dt.datetime.now()
+            self.signal = True
         return self
